a8e4c6ed674e65e01ea45d2aa4fb97d1f2ff4e61
msptc <31194857+msptc@users.noreply.github.com>
2018-02-28 16:54:32 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mysql-homework
+An interactive storefront that runs in the console using node
+
+How It Works
+
+This program consists of an app called bamazonCustomer.js, allows a "customer" to place an order from the Bamazon store. 
+A check is then performed on the store inventory and the customer is either alerted that their order cannot be completed, or 
+they are shown the total amount owed. When a customer makes a transaction on Bamazon, the quantity ordered by 
+the customer is deducted from the store quantity that is stored in a SQL table. The images folder shows what the app looks like in action.
--- commands.sql
+++ commands.sql
@@ -1,20 +1,24 @@
+DROP DATABASE IF EXISTS Bamazon;
 CREATE DATABASE Bamazon;
+USE Bamazon;
 
-USE Bamazon
+CREATE TABLE Products (
+    ItemID INT PRIMARY KEY AUTO_INCREMENT, 
+    productname VARCHAR(30), 
+    departmentname VARCHAR(30), 
+    price DECIMAL(6,2), 
+    StockQuantity INT);
 
-CREATE TABLE Products (ItemID int AUTO-INCREMENT PRIMARY KEY, ProductName VARCHAR(30), 
-DepartmentName VARCHAR(30), Price DECIMAL(6,2), StockQuantity int);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('T-Rex', 'Dinosaurs', 20.75, 7);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Tennis Racket', 'Sporting Goods', 200.57, 29);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('A Brand New Car', 'Auto', 1200.01, 2);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Cat', 'Pets', 76.52, 13);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Couch', 'Furniture', 500.79, 17);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Dishwasher', 'Appliances', 326.75, 24);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Bag of Chips', 'Groceries', 0.50, 2);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('The Great Gatsby', 'Books', 13.57, 7);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Fedora', 'Haberdashery', 175.88, 12);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Invisaline', 'Orthodontic', 999.999, 42);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Mystery Box', 'Mysteries', 100.00, 1);
 
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-INSERT INTO Products(productname, departmentname, price, stockquantity) VALUES();
-
-SELECT * FROM products;
+SELECT * FROM Products;
